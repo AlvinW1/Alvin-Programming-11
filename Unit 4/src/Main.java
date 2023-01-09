1d3a2f59352faf6aae07fb69cb8e749223a78566
AlvinW1 <115210994+AlvinW1@users.noreply.github.com>
2023-01-09 00:25:40 -0800

    School Project

---
+++ Unit 4/src/Main.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Teacher> teachers = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+
+        /* Here the  Teacher list is created and their first name, last name, and Subject is typed in so that
+        * it says their information when it's printed out  */
+        System.out.println("Teachers: ");
+        Teacher Susan = new Teacher("Susan", "Ko", "Math");
+        teachers.add(Susan);
+
+        Teacher Klop = new Teacher("Klop", "Lop", "Social Studies");
+        teachers.add(Klop);
+
+        Teacher Paiza = new Teacher("Paiza", "Qrtt", "Chemistry");
+        teachers.add(Paiza);
+
+        /* The code then takes all the stored teachers in the Arraylist and prints them out */
+        for (int i = 0; i < teachers.size(); i++) {
+            System.out.println(teachers.get(i));
+        }
+        System.out.println();
+        System.out.println("Students:");
+
+        /* Here the  Students list is created and their first name, last name, and Grade is typed in so that
+         * it says their information when it's printed out  */
+        Student Joe = new Student("Joe", "Ho", "7");
+        students.add(Joe);
+
+        Student Matt = new Student("Matt", "Dsad", "9");
+        students.add(Matt);
+
+        Student Maddie = new Student("Maddie", "Sal", "8");
+        students.add(Maddie);
+
+        Student Donald = new Student("Donald", "Pop", "12");
+        students.add(Donald);
+
+        Student Katie = new Student("Katie", "Mcdonald", "11");
+        students.add(Katie);
+
+        Student Sarah = new Student("Sarah", "Dow", "12");
+        students.add(Sarah);
+
+        Student Anthony = new Student("Anthony", "Am", "10");
+        students.add(Anthony);
+
+        Student Dylan = new Student("Dylan", "Lo", "7");
+        students.add(Dylan);
+
+        Student Rachel = new Student("Rachel", "Siu", "11");
+        students.add(Rachel);
+
+        Student Hi = new Student("Hi", "Bye", "8");
+        students.add(Hi);
+
+        /* The code then takes all the stored Students in the Arraylist and prints them out */
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
+        /* This code removes 2 Students and 1 Teacher from the list */
+        students.remove(Katie);
+        students.remove(Sarah);
+        teachers.remove(Susan);
+
+        System.out.println();
+
+        /*This prints out the new Teachers list after the teacher gets removed */
+        System.out.println("Updated Teachers list:");
+        for (int i = 0; i < teachers.size(); i++) {
+            System.out.println(teachers.get(i));
+        }
+        System.out.println();
+
+        /* This prints out the new Students list after the teacher gets removed*/
+            System.out.println("Updated Students list:");
+            for (int i = 0; i < students.size(); i++) {
+                System.out.println(students.get(i));
+
+
+            }
+        }
+    }
+
+
+
+
+
---
+++ Unit 4/src/Teacher.java
@@ -0,0 +1,57 @@
+public class Teacher {
+
+/* These are all the strings that are used in the Teacher class to store information, and they are set to private so
+that it cant be accessed elsewhere */
+    private String firstName;
+    private String lastName;
+   private String subject;
+
+   /* This saves the code so whatever I type in to assign the Strings they are saved to the specific thing I typed in */
+Teacher(){
+    firstName = "";
+    lastName = "";
+    subject = "";
+}
+
+/* This code is so that I can add information to the Teacher string by typing whatever into the allocated slots in the
+main class */
+Teacher(String firstName, String lastName, String subject){
+this.firstName = firstName;
+this.lastName = lastName;
+this.subject = subject;
+
+/* These are the getters and setters for the private Strings */
+}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+
+/* This is the code so when the computer prints out a Teachers name it will follow this format to display the message */
+public String toString(){
+    return "Name:" + this.firstName +" " + this.lastName +", " + "Subject:"  + this.subject;
+}
+    }
+
+
+
